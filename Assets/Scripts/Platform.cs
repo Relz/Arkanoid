627cb06e2e17253bfeb3c3c133b1b9a4600a938c
Relz <relz0071@gmail.com>
2017-03-20 03:57:07 +0300

    Платформа изменяет цвет при том или ином кол-ве HP. При достижении 0 HP платформа уничтожается.

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -2,8 +2,29 @@
 
 public class Platform : MonoBehaviour 
 {
+	void Start()
+	{
+		m_renderer = gameObject.GetComponent<Renderer>();
+				m_renderer.material.color = new Color(1, 0, 0, 1);
+	}
+
 	void OnCollisionEnter(Collision col)
 	{
-		Debug.Log(gameObject.name);
+		--hp;
+		switch (hp)
+		{
+			case 2:
+				m_renderer.material.color = new Color(0, 1, 0, 1);
+				break;
+			case 1:
+				m_renderer.material.color = new Color(0, 0, 1, 1);
+				break;
+			case 0:
+				Destroy(gameObject);
+				break;
+		}
 	}
+
+	private Renderer m_renderer;
+	public uint hp = 3;
 }
